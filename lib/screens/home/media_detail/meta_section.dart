1b6c56e5a3bc99fc2b5b4b8fc2425f201e68897a
Ashish <ashishkumar468@gmail.com>
2019-06-23 18:16:01 +0530

    Ui fixes in media detail, updated primary color and primary color dark

--- lib/screens/home/media_detail/meta_section.dart
+++ lib/screens/home/media_detail/meta_section.dart
@@ -1,4 +1,5 @@
 import 'package:commons/model/response/media/contributions.dart';
+import 'package:commons/utils/misc_util.dart';
 import 'package:flutter/material.dart';
 
 class MetaSection extends StatelessWidget {
@@ -8,19 +9,42 @@ class MetaSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var latitude = null!=media.extmetadata.gPSLatitude?media.extmetadata.gPSLatitude.value:null;
+    var longitude = null!=media.extmetadata.gPSLongitude?media.extmetadata.gPSLongitude.value:null;
+    var name = media.name;
+    var licenseUrl = media.extmetadata.licenseUrl.value.toString();
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-      Text(
-      "Media Details",
-    ),
-    Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      height: 8.0,
-    ),
-    _getSectionOrContainer('Title', media.title),
-    _getSectionOrContainer('Description', media.descriptionurl),
-    ],
+        new Container(
+          padding: const EdgeInsets.all(10.0),
+          color: hexToColor("#0c609c"),
+          child: getTitleSection("Title: $name"),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          height: 10.0,
+        ),
+        new GestureDetector(
+            onTap: () => openLinkInWebBrowser(licenseUrl),
+            child: _getSectionOrContainer(
+                'License', media.extmetadata.licenseShortName.value,
+                isLink: true)),
+        _getSectionOrContainer(
+            'Description', media.extmetadata.imageDescription.value),
+        _getSectionOrContainer(
+            'Coordinates',
+            "$latitude,$longitude".contains("null")
+                ? "NA"
+                : "$latitude,$longitude"),
+        _getSectionOrContainer(
+            'Categories',
+            getCategoryDisplayableString(
+                media.extmetadata.categories.toJson())),
+        _getSectionOrContainer('Uploaded Date',
+            formatDate(media.extmetadata.dateTime.value).toString())
+        //TODO, format date time,
+      ],
     );
   }
 
@@ -28,12 +52,9 @@ class MetaSection extends StatelessWidget {
       {dynamic formatterFunction, bool isLink: false}) {
     return content == null
         ? Container()
-        : _getMetaInfoSection(
-        title,
-        (formatterFunction != null
-            ? formatterFunction(content)
-            : content),
-        isLink: isLink);
+        : _getMetaInfoSection(title,
+            (formatterFunction != null ? formatterFunction(content) : content),
+            isLink: isLink);
   }
 
   Widget _getMetaInfoSection(String title, String content,
@@ -41,7 +62,7 @@ class MetaSection extends StatelessWidget {
     if (content == null) return Container();
 
     var contentSection = Expanded(
-      flex: 10,
+      flex: 6,
       child: GestureDetector(
         child: Text(
           content,
@@ -52,17 +73,39 @@ class MetaSection extends StatelessWidget {
     );
 
     return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Text(
+                '$title:',
+                style: TextStyle(
+                    color: hexToColor("#0c609c"),
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            contentSection
+          ],
+        ));
+  }
+
+  getTitleSection(String title) {
+    return Padding(
         padding: const EdgeInsets.symmetric(vertical: 10.0),
         child: Row(
           children: <Widget>[
             Expanded(
-              flex: 10,
+              flex: 4,
               child: Text(
-                '$title:',
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
+                '$title',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
               ),
             ),
-            contentSection
           ],
         ));
   }
