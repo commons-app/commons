06ad95b6e5cd15a7f77d440e5445268abc522656
Vivek Maskara <maskaravivek@gmail.com>
2019-06-30 12:27:15 +0530

    with minor changes

--- .travis/40_postbuild.sh
+++ .travis/40_postbuild.sh
@@ -4,5 +4,5 @@ STATUS="$1"
 BUILD_OUTPUT_URL =`node ./.travis/utils/runUpload.js`
 
 curl -H "Authorization: token ${TRAVIS_COMMENT_PERSONAL_TOKEN}" -X POST \
-          -d "{\"body\": \"Get the APK for Job #'"$TRAVIS_JOB_NUMBER"' (Build #'"$TRAVIS_BUILD_NUMBER"') at BUILD_OUTPUT_URL\"}" \
+          -d "{\"body\": \"Get the APK for Job #$TRAVIS_JOB_NUMBER (Build #$TRAVIS_BUILD_NUMBER) at $BUILD_OUTPUT_URL\"}" \
           "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
